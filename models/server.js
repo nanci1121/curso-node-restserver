2e2180cfd9313236a0e8d88065a8a4791ec30637
Venancio <mavipi@gmail.com>
2021-02-19 12:13:09 +0100

    Usuarios CRUD list 19-02-21

--- models/server.js
+++ models/server.js
@@ -1,39 +1,56 @@
-var express = require('express');
-var cors = require('cors');
-// var app = express();
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('../database/config');
 
 class Server {
+
     constructor() {
-        this.app = express();
+        this.app  = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
-        //Midlewares
+
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Middlewares
         this.middlewares();
-        //Rutas de mi aplicacion
+
+        // Rutas de mi aplicación
         this.routes();
     }
 
+    async conectarDB() {
+        await dbConnection();
+    }
+
+
     middlewares() {
-        //Directorio Publico
-        this.app.use(express.static('public'));
-        //Cors
-        this.app.use(cors());
-        //Lectura y parseo del Body
-        this.app.use(express.json());
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
 
     }
-    routes() {
-        this.app.use(this.usuariosPath, require('../routes/user'));
 
+    routes() {
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
     }
 
     listen() {
-        this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port);
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
         });
     }
 
 }
+
+
 
 
 module.exports = Server;
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 const {
     usuariosGet,
     usuariosPost,
@@ -10,7 +13,20 @@ const {
 const router = Router();
 
 router.get('/', usuariosGet);
-router.post('/', usuariosPost);
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('pasword', 'El password tiene que tener mas de 6 letras y es obligatorio').isLength({ min: 6 }),
+    check('correo', 'El valor ingresado no tiene el valor adecuado').isEmail(),
+    // check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(async(rol = '') => {
+        const existeRol = await Role.findOne({ rol });
+        if (!existeRol) {
+            throw new Error(`El rol ${ rol } no esta registrado en la DB `);
+        }
+    }),
+    validarCampos
+], usuariosPost);
+
 router.put('/:id', usuariosPut);
 router.patch('/', usuariosPatch);
 router.delete('/', usuariosDelete);
